0bd90fa6e2e0ed5fdf1082f93f71748b3efd1486
Jens Jensen <jej@eucnord.dk>
2022-04-21 10:03:28 +0200

    Add project files.

---
+++ Maskine/Terning.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maskine
+{
+    public class Terning
+    {
+        public int AntalSider;
+        public int Værdi;
+
+        public int Kast()
+        {
+            Random random = new Random();
+            int value = random.Next(1, AntalSider+1);
+            Værdi = value; 
+            return value;
+        }
+    }
+}
---
+++ MineTerninger/Program.cs
@@ -0,0 +1,17 @@
+﻿using Maskine;
+// Lav nye terninger
+Terning terning1 = new Terning();
+Terning terning2 = new Terning();
+// Initialiser terningerne
+terning1.AntalSider = 6;
+terning2.AntalSider = 69;
+// Kast med terningerne
+terning1.Kast();
+terning2.Kast();
+// Udskriv resultat
+
+Console.Write(terning1.Værdi);
+Console.Write(" ");
+Console.Write(terning2.Værdi);
+Console.Write(" ");
+Console.WriteLine(terning1.Værdi + terning2.Værdi);
